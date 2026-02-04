cce0a24f97b7c2e57ccafcaf7fa75ec2f141b60c
abdul <itsmymail05@mail.ru>
2026-02-05 00:16:53 +0300

    -

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -8,7 +10,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
-    passsword = db.Column(db.String(128), nullable=False)
+    passsword_hash = db.Column(db.String(128), nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
 
 @app.route("/login")
